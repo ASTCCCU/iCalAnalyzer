c78eec8cf81fb9f1f85c8a0ec96c8e5dbcac2d32
King Tin Lam <kingtinlam@gmail.com>
2017-10-31 15:48:18 +0800

    Added TestOnly.java
    Modified MyCalendar.java

--- src/MyCalendar.java
+++ src/MyCalendar.java
@@ -2,4 +2,9 @@
 public class MyCalendar {
 	public String date;
 	public String notes;
+        
+        public MyCalendar() {
+            date = "today";
+            notes = "something";
+        }
 }
