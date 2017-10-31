54992b02b172042c1144691b018fa66c1eff63ba
King Tin Lam <kingtinlam@gmail.com>
2017-10-31 13:09:31 +0800

    Added HelloWorld

---
+++ src/HelloWord.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author kingtin
+ */
+public class HelloWord {
+    private String name;
+    private int count;
+}
